81e667f4177323e3691487010e629c17ef1951c3
81723-wen <qinwenhu2024@outlook.com>
2025-09-24 21:25:09 +0800

    Add files via upload
    Day2 挑战1：用户自定义数量

--- day2_contract_gen.py
+++ day2_contract_gen.py
@@ -1,8 +1,12 @@
 #Day2 合同编号生成器
+num = int(input('生成几份？请输入数字：'))
+
 with open('contracts.txt','w',encoding='utf-8') as f:
-    for i in range(1,11):
+    for i in range(1,num + 1):        # 1到 num(含)
         contract_id = f'2025-HT-{i:03d}'
         f.write(contract_id + '\n')   #每行一个编号
         print(contract_id)
 
 print('10份合同编号已写入contracts.txt, 请验收～')
+
+
